f2fd53f7e72dd20762b7629423101f59e95b4eb2
Ryan Carlos <ryancarlos925@gmail.com>
2022-05-06 18:07:20 -0700

    commit tree 124fda73ea992fef8e5246b0c7a82492a0762c86

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+       // record.find(r=>r.result==="W").year)
+    function superbowlWin(record){
+         let r = record.find(r=>r.result==="W")
+
+          return !!r ? r.year : undefined;
+
+     }
+//             Action Plan:
+// 1. use console.log to see if an object is stored in r
+// 2. create an if statement to do this:  
+// - It should return the year when the win occurred (if it occurred at all!)   
+// - If no win is found, it should return, sadly, undefined\
